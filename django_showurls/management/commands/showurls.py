2fc18bdcbffd3e671b3684931fb7a88c22f9e8d4
Niklas Andersson <nandersson900@gmail.com>
2016-02-20 09:26:00 +0000

    fixed some magic variables

--- django_showurls/management/commands/showurls.py
+++ django_showurls/management/commands/showurls.py
@@ -14,14 +14,22 @@ urls = importlib.import_module('%s.urls' % project_name)
 
 class Command(base.BaseCommand):
 
+    DEFAULT_INDENTATION = '  '
+    DEFAULT_INDENTATION_DEPTH = 1
+    START_INDENTATION_DEPTH = 0
+    PRINT_URL_FORMAT = '%s%s'
+    URL_ENTRY_PROPERTY = 'url_patterns'
+
     help = 'Outputs all configured urls in a nice format'
 
     def handle(self, *args, **kwargs):
-        self._print_urls(urls.urlpatterns, depth=0)
+        self._print_urls(urls.urlpatterns, depth=self.START_INDENTATION_DEPTH)
 
     @staticmethod
-    def _print_urls(urllist, depth=0):
+    def _print_urls(urllist, depth=START_INDENTATION_DEPTH):
         for entry in urllist:
-            print('%s%s' % ('  '* depth, entry.regex.pattern))
-            if hasattr(entry, 'url_patterns'):
-                Command._print_urls(entry.url_patterns, depth + 1)
+            indent_s = Command.DEFAULT_INDENTATION * depth
+            print(Command.PRINT_URL_FORMAT % (indent_s, entry.regex.pattern))
+            if hasattr(entry, Command.URL_ENTRY_PROPERTY):
+                new_depth = depth + Command.DEFAULT_INDENTATION_DEPTH
+                Command._print_urls(entry.url_patterns, depth=new_depth)
